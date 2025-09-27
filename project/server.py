f22033a1fb85bd9d08d59df94f2d736498216e88
Shanreed25 <shannontlreed@gmail.com>
2025-09-26 19:00:21 -0700

    added the main nav items for the dashboard dynamically from the server

--- project/server.py
+++ project/server.py
@@ -8,16 +8,46 @@ def home():
 
 @app.route("/dashboard/<language>")
 def language_dashboard(language):
+    # Main Navigation Setup
+
     if language not in ["html", "css", "javascript", "python"]:
         return "Language not supported", 404
     if language == "python":
-        return render_template("dashboard/python/python-home.html")
+        mainNavItems = {# Python Main Navigation Setup
+            "dashboard_title": "Python",
+            "nav_item1": "HTML",
+            "nav_item2": "CSS",
+            "nav_item3": "JavaScript",
+            }
+        return render_template("dashboard/python/python-home.html",
+                                **mainNavItems)
     if language == "javascript":
-        return render_template("dashboard/javascript/js-home.html")
+        mainNavItems = {# Javascript Main Navigation Setup
+            "dashboard_title": "JavaScript",
+            "nav_item1": "HTML",
+            "nav_item2": "CSS",
+            "nav_item3": "Python",
+            }
+        return render_template("dashboard/javascript/js-home.html",
+                                **mainNavItems)
     if language == "css":
-        return render_template("dashboard/css/css-home.html")
+        mainNavItems = {# CSS Main Navigation Setup
+            "dashboard_title": "CSS",
+            "nav_item1": "HTML",
+            "nav_item2": "JavaScript",
+            "nav_item3": "Python",
+            }
+        return render_template("dashboard/css/css-home.html",
+                                **mainNavItems)
     if language == "html":
-        return render_template("dashboard/html/html-home.html")
+        mainNavItems = {# HTML Main Navigation Setup
+            "dashboard_title": "HTML",
+            "nav_item1": "CSS",
+            "nav_item2": "JavaScript",
+            "nav_item3": "Python",
+            }
+        return render_template("dashboard/html/html-home.html",
+                                **mainNavItems)
 
 if __name__ == "__main__":
     app.run(debug=True)
